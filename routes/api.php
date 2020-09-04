fb2bb90094ed566be029209762dc6260270e72f3
Thulio Guirelle Horta <Yoostah@users.noreply.github.com>
2020-09-04 15:44:02 -0300

    Route Creation

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,54 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/live', function(){
+    return ['status' => 'OK'];
 });
+
+Route::get('/401', 'AuthController@unauthorized')->name('login');
+
+/** ACESSO SEM TOKEN **/
+Route::post('/auth/login', 'AuthController@login');
+Route::post('/user', 'UserController@store');
+
+/** ACESSO COM TOKEN **/
+Route::post('/auth/logout', 'AuthController@logout');
+Route::post('/auth/refresh', 'AuthController@refresh');
+
+## USUÁRIOS ##
+Route::get('/user/list', 'UserController@index');
+Route::get('/user', 'UserController@show');
+Route::get('/user/{id}', 'UserController@show');
+Route::delete('/user/{id}', 'UserController@destroy');
+Route::put('/user/{id}/admin', 'UserController@isAdmin');
+Route::put('/user', 'UserController@update');
+Route::post('/user/avatar', 'UserController@updateAvatar');
+
+## STATUS ##
+Route::get('/status', 'StatusControler@index');
+Route::get('/status/{id}', 'StatusControler@show');
+Route::post('/status', 'StatusControler@store');
+Route::delete('/status/{id}', 'StatusControler@destroy');
+Route::put('/status', 'StatusControler@update');
+
+## ITEMS ##
+Route::get('/item', 'ItemControler@index');
+Route::get('/item/{id}', 'ItemControler@show');
+Route::post('/item', 'ItemControler@store');
+Route::delete('/item/{id}', 'ItemControler@destroy');
+Route::put('/item', 'ItemControler@update');
+
+## CATEGORIA DE PRODUTOS ##
+Route::get('/category', 'CategoryControler@index');
+Route::get('/category/{id}', 'CategoryControler@show');
+Route::post('/category', 'CategoryControler@store');
+Route::delete('/category/{id}', 'CategoryControler@destroy');
+Route::put('/category', 'CategoryControler@update');
+
+## STOCK ##
+Route::get('/stock/{id}', 'StockControler@show');
+Route::post('/stock', 'StockControler@store');
+Route::put('/stock', 'StockControler@update');
+
+## BUSCA ##
+Route::get('/search', 'SearchControler@search');
